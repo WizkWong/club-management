becbdf72b62709f5f66e0f62bcccc66c21784976
WizkWong <chijianwong7@gmail.com>
2022-04-05 22:58:25 +0800

    add some setting

--- club_management/blog/views.py
+++ club_management/blog/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Chi Jian',
+        'title': 'Test1',
+        'content': 'First post',
+        'date_posted': 'March 31, 2022'
+    },
+    {
+        'author': 'Chi Jian',
+        'title': 'Test2',
+        'content': 'Second post',
+        'date_posted': 'April 1, 2022'
+    }
+]
+
+data = [
+    'Bing', 'Google', 'Firefox'
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Home<h1>')
+    context = {
+        'profile': posts,
+        'title': 'home'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About<h1>')
+    context = {
+        'types': data
+    }
+    return render(request, 'blog/about.html', context)

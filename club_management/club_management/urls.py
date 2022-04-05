29f8fe01043e2ea9a96c6b9e0076d8a36aec5ec9
WizkWong <chijianwong7@gmail.com>
2022-04-05 22:58:25 +0800

    add some setting

--- club_management/club_management/urls.py
+++ club_management/club_management/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# include(file path)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]

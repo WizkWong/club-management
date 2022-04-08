45f2043ae49e4fda9bcff788c9902880a7ddd84f
WizkWong <chijianwong7@gmail.com>
2022-04-08 16:46:43 +0800

    add register, login and logout part

--- club_management/research.py
+++ club_management/research.py
@@ -1,2 +1,4 @@
-print("first project")
-print('hi')
+from django.contrib.auth.models import User
+
+def object_all():
+    return User.objects.all()
--- club_management/users/views.py
+++ club_management/users/views.py
@@ -1,15 +1,50 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            # user = form.save()
+            # login(request, user)
             messages.success(request, f'Account created for {username}!')
             return redirect('club-home')
     else:
         form = UserCreationForm()
 
-    return render(request, 'users/register.html', {'form':form})
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("club-home")
+            else:
+                messages.error(request, "Invalid username or passworddhdh.")
+
+        else:
+            messages.error(request, "Invalid username or password")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("club-home")

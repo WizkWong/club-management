71302e08dcab94a269a92e7393ce15947b727fe1
WizkWong <chijianwong7@gmail.com>
2022-04-08 17:30:39 +0800

    add profile

--- club_management/users/views.py
+++ club_management/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 
@@ -48,3 +49,8 @@ def logout_request(request):
     logout(request)
     messages.info(request, "You have successfully logged out.")
     return redirect("club-home")
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')

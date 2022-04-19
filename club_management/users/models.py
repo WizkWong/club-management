0290436d62ea65464b5c8c486b72c41a4a264570
WizkWong <chijianwong7@gmail.com>
2022-04-19 15:32:40 +0800

    modify the database and profile models

--- club_management/users/models.py
+++ club_management/users/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 
 class profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    description = models.TextField(null=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):

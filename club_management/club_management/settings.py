89c5b81e57032cf7cfbdb1c5b1eae99675f1a867
WizkWong <chijianwong7@gmail.com>
2022-05-09 20:54:26 +0800

    configure setting and add the user url

--- club_management/club_management/settings.py
+++ club_management/club_management/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'club.apps.ClubConfig',
     'users.apps.UsersConfig',
+    'admins.apps.AdminsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- club_management/club_management/urls.py
+++ club_management/club_management/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('club.urls')),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    path('admins/', include('admins.urls'))
 ]
 
 if settings.DEBUG:

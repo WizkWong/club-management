2e9454769d8251a44c9ed71b61e1c11294111506
WizkWong <chijianwong7@gmail.com>
2022-05-10 21:25:34 +0800

    add all the database in admin and user

--- club_management/users/models.py
+++ club_management/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
+from admins.models import Task
 
 
 class profile(models.Model):
@@ -9,3 +11,20 @@ class profile(models.Model):
 
     def __str__(self):
         return f'Profile: {self.user.username}'
+
+
+class User_request(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    datetime_created = models.DateTimeField(default=timezone.now)
+    detail = models.TextField()
+
+
+class Task_assigned(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    upload_file = models.FileField(null=True, upload_to='files')
+    complete = models.BooleanField(default=False)
+    datetime_complete = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = (('task', 'user'),)

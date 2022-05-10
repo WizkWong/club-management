c6ddd94b3eea1c20192699069e446eb957961344
WizkWong <chijianwong7@gmail.com>
2022-05-10 23:28:01 +0800

    add some code in the admin.py of users

--- club_management/users/admin.py
+++ club_management/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import profile, User_request, Task_assigned
+from .models import profile, User_request, Task_assigned, Chat, Pending_user
 
 admin.site.register(profile)
 admin.site.register(User_request)
 admin.site.register(Task_assigned)
+admin.site.register(Chat)
+admin.site.register(Pending_user)

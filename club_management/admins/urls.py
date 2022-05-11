1ecc49b4e5fd4e859d27cf506cb594aa5a988a97
WizkWong <chijianwong7@gmail.com>
2022-05-11 17:22:16 +0800

    add something to admin edit user and some changes

--- club_management/admins/urls.py
+++ club_management/admins/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('attendance/', views.manage_attendance, name='admin-attendance'),
     path('task/', views.manage_task, name='admin-task'),
     path('all_users/', views.manage_user, name='admin-user'),
+    path('all_users/<str:pk>', views.edit_user, name='admin-user-edit'),
     path('request/', views.manage_request, name='admin-request'),
     path('report/', views.manage_report, name='admin-report'),
     path('edit_page/', views.edit_page, name='admin-edit-page'),
--- club_management/admins/views.py
+++ club_management/admins/views.py
@@ -1,9 +1,20 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.forms import UserUpdateForm, ProfileUpdateForm
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
+
+def permission(request):
+    if request.user.is_superuser is False:
+        raise Http404("Page Not Found")
 
 
 @login_required
 def manage_attendance(request):
+    permission(request)
     content = {
         'title': 'Manage Attendance'
     }
@@ -12,6 +23,7 @@ def manage_attendance(request):
 
 @login_required
 def manage_task(request):
+    permission(request)
     content = {
         'title': 'Manage Task'
     }
@@ -20,14 +32,46 @@ def manage_task(request):
 
 @login_required
 def manage_user(request):
+    permission(request)
     content = {
-        'title': 'Manage User'
+        'title': 'Manage User',
+        'all_user': User.objects.filter(is_superuser=False)
     }
     return render(request, 'admins/user.html', content)
 
 
 @login_required
+def edit_user(request, pk):
+    permission(request)
+    user = get_object_or_404(User, Q(is_superuser=False), username=pk)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('admin-user')
+
+    else:
+        u_form = UserUpdateForm(instance=user)
+        p_form = ProfileUpdateForm(instance=user.profile)
+
+    content = {
+        'title': 'Manage User',
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'admins/user edit.html', content)
+
+
+
+@login_required
 def manage_request(request):
+    permission(request)
     content = {
         'title': 'Manage Request'
     }
@@ -36,6 +80,7 @@ def manage_request(request):
 
 @login_required
 def manage_report(request):
+    permission(request)
     content = {
         'title': 'Manage Report'
     }
@@ -44,6 +89,7 @@ def manage_report(request):
 
 @login_required
 def edit_page(request):
+    permission(request)
     content = {
         'title': 'Edit Page'
     }

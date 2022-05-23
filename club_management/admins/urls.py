3722451ebc690f6c480295cefd8d5d3b56ecf5aa
WizkWong <chijianwong7@gmail.com>
2022-05-23 13:27:40 +0800

    edit style name part

--- club_management/admins/urls.py
+++ club_management/admins/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('users/<str:pk>/delete/', views.delete_user, name='admin-user-delete'),
     path('request/<str:types>/', views.manage_request, name='admin-request'),
     path('request/<str:types>/<int:pk>/', views.view_request_detail, name='admin-request-detail'),
-    path('request/<str:types>/<int:pk>/delete', views.delete_request, name='admin-request-delete'),
+    path('request/<str:types>/<int:pk>/delete/', views.delete_request, name='admin-request-delete'),
     path('report/', views.manage_report, name='admin-report'),
     path('edit_page/', views.edit_page, name='admin-edit-page'),
 ]

4e9175fd26ccebc209f88350a505c01cebf8faf6
WizkWong <chijianwong7@gmail.com>
2022-05-29 23:00:43 +0800

    add some part for admin task

--- club_management/admins/forms.py
+++ club_management/admins/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from .models import Request_feedback
+from .models import Request_feedback, Task
+
+
+class DateTimePickerInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
 
 
 class RequestFeedbackForm(forms.ModelForm):
@@ -15,3 +19,22 @@ class RequestFeedbackForm(forms.ModelForm):
     class Meta:
         model = Request_feedback
         fields = ['approval', 'feedback']
+
+
+class TaskForm(forms.ModelForm):
+    detail = forms.CharField(required=False,
+                             widget=forms.Textarea(
+                                 attrs={
+                                     "rows": 10,
+                                     "cols": 70,
+                                     "class": "field"
+                                 }
+                             ))
+
+    class Meta:
+        model = Task
+        fields = ['title', 'detail', 'deadline']
+        widgets = {
+            'title': forms.TextInput(attrs={'size': 60}),
+            'deadline': DateTimePickerInput(),
+        }

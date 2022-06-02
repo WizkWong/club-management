8037690420fcc50e485ee0fb4c14f261ba6f5281
WizkWong <chijianwong7@gmail.com>
2022-06-02 23:31:48 +0800

    change the string url path in signals.py

--- club_management/admins/signals.py
+++ club_management/admins/signals.py
@@ -1,10 +1,11 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from users.models import Task_assigned
+from club_management.settings import MEDIA_URL
 import os
 
 
 @receiver(pre_delete, sender=Task_assigned)
 def delete_profile(sender, instance, **kwargs):
-    if os.path.exists(f'media/{instance.upload_file}'):
-        os.remove(f'media/{instance.upload_file}')
+    if os.path.exists(f'{MEDIA_URL.replace("/", "")}/{instance.upload_file}'):
+        os.remove(f'{MEDIA_URL.replace("/", "")}/{instance.upload_file}')

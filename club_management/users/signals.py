4b8ca2328231c0230c6062ceb019e424f7b6499c
WizkWong <chijianwong7@gmail.com>
2022-06-07 23:56:00 +0800

    fix the bug

--- club_management/users/signals.py
+++ club_management/users/signals.py
@@ -1,7 +1,7 @@
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, pre_delete, pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import profile, User_request
+from .models import profile, User_request, Task_assigned
 from admins.models import Request_feedback
 from club_management.settings import MEDIA_URL
 import os
@@ -22,12 +22,35 @@ def delete_profile(sender, instance, **kwargs):
             os.remove(f'{MEDIA_URL.replace("/", "")}/{instance.profile.image}')
 
 
-# @receiver(post_save, sender=User)
-# def save_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(pre_save, sender=User)
+def replace_profile(sender, instance, **kwargs):
+    if str(instance.profile.image).startswith('profile_pics'):
+        return
+
+    else:
+        old = User.objects.get(id=instance.id)
+        if old.profile.image == 'default.jpg':
+            return
+        else:
+            if os.path.exists(f'{MEDIA_URL.replace("/", "")}/{old.profile.image}'):
+                os.remove(f'{MEDIA_URL.replace("/", "")}/{old.profile.image}')
 
 
 @receiver(post_save, sender=User_request)
 def create_request_feedback(sender, instance, created, **kwargs):
     if created:
         Request_feedback.objects.create(request=instance, user=instance.user)
+
+
+@receiver(pre_save, sender=Task_assigned)
+def replace_save_file(sender, instance, **kwargs):
+    if str(instance.upload_file).startswith('files'):
+        return
+
+    else:
+        old_file = Task_assigned.objects.get(task=instance.task, user=instance.user)
+        if old_file.upload_file:
+            if os.path.exists(f'{MEDIA_URL.replace("/", "")}/{old_file.upload_file}'):
+                os.remove(f'{MEDIA_URL.replace("/", "")}/{old_file.upload_file}')
+        else:
+            return

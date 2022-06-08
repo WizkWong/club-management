8acd0e9a405b6c65cc03c8114984908b9b4f607b
WizkWong <chijianwong7@gmail.com>
2022-06-08 18:54:56 +0800

    clean up

--- club_management/users/models.py
+++ club_management/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from admins.models import Task
-from django.urls import reverse
 
 
 class profile(models.Model):
